5aa6653fa3575f91321d78c60dfdbc8886ce5588
codetective <iaminiodu@gmail.com>
2021-09-08 13:21:41 +0100

    added scrolling collage

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Box, Image } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-// import collage from '../assets/babynftcollage.jpg';
-import collage from '../assets/tenbabies.jpg';
 function Layout({ children }) {
   return (
     <Box maxW="1500px" m="auto">
       <Navbar />
 
       {children}
-      <Box>
-        <Box
-          m="auto"
-          //  w={['full', 'full', 'full', '80%']}
-        >
-          <Image src={collage} />
-        </Box>
+      <Box m="auto" className="beforeFooterBox" w="100%">
+        {/* <Image src={collage} /> */}
       </Box>
       <Footer />
     </Box>
